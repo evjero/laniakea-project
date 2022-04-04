62fe0367812ef82695d084cb4c14ebb4c4fe57a9
epsnaught <the.epsilon.naught@gmail.com>
2022-04-03 21:30:36 -0400

    Stub tests; need mock server or interceptor; look into databases next

---
+++ server/src/mocks/handlers.js
@@ -0,0 +1,48 @@
+import { rest } from 'msw';
+export const handlers = [
+	rest.get('/planets', (req, res, ctx) => {
+		return res(
+			ctx.status(200).json([
+				{
+					kepoi_name: 'Ktest1',
+					kepler_name: 'test1',
+					koi_disposition: 'CONFIRMED',
+				},
+				{
+					kepoi_name: 'Ktest2',
+					kepler_name: 'test2',
+					koi_disposition: 'FALSE POSITIVE',
+				},
+			])
+		);
+	}),
+	rest.get('/planets?habitable=true', (req, res, ctx) => {
+		return res(
+			ctx.status(200).json([
+				{
+					kepoi_name: 'Ktest1',
+					kepler_name: 'test1',
+					koi_disposition: 'CONFIRMED',
+				},
+			])
+		);
+	}),
+	rest.get('/launches', (req, res, ctx) => {
+		return res(
+			ctx.status(200).json([
+				{
+					destination: 'Ktest1',
+					flightNumber: 1,
+					launchDate: new Date('2032-04-01'),
+					mission: 'Spectacle',
+					rocket: 'Saturn VIII',
+					success: false,
+					upcoming: true,
+				},
+			])
+		);
+	}),
+	rest.post('/launches', (req, res, ctx) => {
+		return res(ctx.status(201));
+	}),
+];
---
+++ server/src/routes/planets/planets.test.js
@@ -0,0 +1,8 @@
+describe('Test GET /planets', () => {
+	test('200', () => {});
+});
+describe('Test POST /planets', () => {
+	test('200', () => {});
+	test('Missing required properties', () => {});
+	test('Invalid dates', () => {});
+});
